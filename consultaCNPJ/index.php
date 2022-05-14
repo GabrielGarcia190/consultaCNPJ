4174164162aaa30c7ed9afcaddf864a52f641f88
Gabriel Garcia <gokugamer190@gmail.com>
2022-05-13 22:09:33 -0300

    Adicionando página HTML

---
+++ consultaCNPJ/index.php
@@ -0,0 +1,59 @@
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title> API Receita Federal</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-md-4">
+
+        </div>
+        <div class="col-md-4">
+            <div class="form-group row">
+                <div class="col-md-12"><br><br>
+                    <label for="CNPJ">CNPJ</label>
+                    <input type="text" class="form-control" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-md-12">
+                    <label for="razaoSocial">Razão Social</label>
+                    <input type="text" class="form-control" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-md-12">
+                    <label for="nomeFantasia">Nome Fantasia</label>
+                    <input type="text" id="nomeFantasia" class="form-control" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-md-8">
+                    <label for="logradouro">Logradouro</label>
+                    <input type="text" id="logradouro" class="form-control" />
+                </div>
+                <div class="col-md-4">
+                    <label for="numero">Número</label>
+                    <input type="text" id="numero" class="form-control" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-md-8">
+                    <label for="municipio">Municipio</label>
+                    <input type="text" id="municipio" class="form-control" />
+                </div>
+                <div class="col-md-4">
+                    <label for="uf">SP</label>
+                    <input type="text" id="uf" class="form-control" />
+                </div>
+            </div>
+            <div class="col-md-4">
+
+            </div>
+        </div>
+</body>
+
+</html>
