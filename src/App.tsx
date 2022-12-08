50076ff6dccc6867d3a6d347b4e307fee81e9fa2
gabriegarcia190 <gokugamer190@gmail.com>
2022-12-08 13:09:23 -0300

    Terminando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,10 @@ import copy from "copy-to-clipboard";
 import { Copy, Warning, X } from "phosphor-react";
 
 Modal.setAppElement("#root");
-interface CNPJProps {
-  razao_social: string;
-  estabelecimento: {
-    nome_fantasia: string;
-    situacao_cadastral: string;
-  };
-  situacao_cadastral: string;
-  tipo_logradouro: string;
-  logradouro: string;
-  numero: string;
-  complemento: string;
-  bairro: string;
-  cep: string;
-  estado: {
-    nome: string;
-    sigla: string;
-  };
-  cidade: {
-    cidade: string;
-  };
-}
 
 function App() {
   const [modalIsOpen, setIsOpen] = useState(false);
   const [modalError, setModalError] = useState(false);
-  const [copySucess, setcopySucess] = useState(false);
 
   const [resAPI, setresAPI] = useState({
     razao_social: "",
@@ -51,13 +29,10 @@ function App() {
       cidade: {
         nome: "",
       },
-      inscricoes_estaduais: [
-        {
-          inscricao_estadual: "",
-        }
-      ]
+      pais: {
+        nome: "",
+      },
     },
-    
   });
 
   const [apiCNPJ, useCNPJ] = useState({
@@ -100,7 +75,11 @@ function App() {
   }
 
   function copyToClipboard(Text: string) {
-    copy(Text);
+    if (Text === null) {
+      copy("*****");
+    } else {
+      copy(Text);
+    }
   }
 
   function closeModal() {
@@ -168,9 +147,9 @@ function App() {
           isOpen={modalIsOpen}
           overlayClassName="modal-overlay"
           contentLabel="modal-resultado"
-          className="flex flex-col rounded-lg  bg-[#E5E7EB] w-[35vw] h-[73vh] shadow-xl"
+          className="flex flex-col rounded-lg  bg-[#E5E7EB] w-[35vw] h-full shadow-xl"
         >
-          <div className="bg-[#E5E7EB] text-[#AAA4A4] mx-auto flex flex-col mt-4 items-center">
+          <div className="bg-[#E5E7EB] text-sm  text-[#AAA4A4] mx-auto flex flex-col mt-4 items-center">
             <div className="flex flex-col items-center justify-center">
               <button onClick={closeModal}>
                 <X size={30} className="text-red-600 ml-[30vw]" weight="bold" />
@@ -179,7 +158,9 @@ function App() {
                 - Resultado da Busca -
               </h1>
             </div>
-            <div className="flex flex-row mt-5 text-center w-[30vw]">
+
+            <div className="flex flex-col mt-5 text-start w-[30vw]">
+              <p className="text-[#303740]">Razao Social:</p>
               <p className="bg-[#CECFD2] p-2 w-full  text-[#303740] flex flex-row items-center justify-between rounded-lg">
                 {resAPI.razao_social}
                 <button onClick={() => copyToClipboard(resAPI.razao_social)}>
@@ -187,9 +168,12 @@ function App() {
                 </button>
               </p>
             </div>
-            <div className="flex flex-row mt-5 text-center w-[30vw]">
+            <div className="flex flex-col mt-5 text-start w-[30vw]">
+              <p className="text-[#303740]">Nome Fantasia:</p>
               <p className="bg-[#CECFD2] p-2 w-full  text-[#303740] flex flex-row items-center justify-between rounded-lg">
-                {resAPI.estabelecimento.nome_fantasia == null ? "******" : resAPI.estabelecimento.nome_fantasia}
+                {resAPI.estabelecimento.nome_fantasia == null
+                  ? "******"
+                  : resAPI.estabelecimento.nome_fantasia}
                 <button
                   onClick={() =>
                     copyToClipboard(resAPI.estabelecimento.nome_fantasia)
@@ -199,83 +183,130 @@ function App() {
                 </button>
               </p>
             </div>
-            <div className="flex flex-row mt-5 text-center w-[30vw]">
-              <p className="bg-[#CECFD2] p-2 w-[24vw]  text-[#303740] flex flex-row items-center justify-between rounded-lg">
-                {`${resAPI.estabelecimento.tipo_logradouro} ${resAPI.estabelecimento.logradouro}`}
-                <button
-                  onClick={() =>
-                    copyToClipboard(resAPI.estabelecimento.logradouro)
-                  }
-                >
-                  <Copy weight="bold" size={25} />
-                </button>
-              </p>
-              <p className="bg-[#CECFD2] p-2 w-[8vw] ml-4 text-[#303740] flex flex-row items-center justify-between rounded-lg">
-                {resAPI.estabelecimento.numero}
-                <button
-                  onClick={() => copyToClipboard(resAPI.estabelecimento.numero)}
-                >
-                  <Copy weight="bold" size={25} />
-                </button>
-              </p>
+            <div className="flex flex-col mt-5 text-start w-[30vw]">
+              <div className="flex">
+                <p className="w-[24vw] text-[#303740]"> Logradouro:</p>
+                <p className=" text-[#303740]"> Número:</p>
+              </div>
+              <div className="flex flex-row">
+                <p className="bg-[#CECFD2] p-2 w-[24vw]  text-[#303740] flex flex-row items-center justify-between rounded-lg">
+                  {`${resAPI.estabelecimento.tipo_logradouro} ${resAPI.estabelecimento.logradouro}`}
+                  <button
+                    onClick={() =>
+                      copyToClipboard(resAPI.estabelecimento.logradouro)
+                    }
+                  >
+                    <Copy weight="bold" size={25} />
+                  </button>
+                </p>
+                <p className="bg-[#CECFD2] p-2 w-[8vw] ml-4 text-[#303740] flex flex-row items-center justify-between rounded-lg">
+                  {resAPI.estabelecimento.numero}
+                  <button
+                    onClick={() =>
+                      copyToClipboard(resAPI.estabelecimento.numero)
+                    }
+                  >
+                    <Copy weight="bold" size={25} />
+                  </button>
+                </p>
+              </div>
             </div>
-            <div className="flex flex-row mt-5 text-center w-[30vw]">
-              <p className="bg-[#CECFD2] p-2 w-[15vw]  text-[#303740] flex flex-row items-center justify-between rounded-lg">
-                {resAPI.estabelecimento.bairro}
-                <button
-                  onClick={() => copyToClipboard(resAPI.estabelecimento.bairro)}
-                >
-                  <Copy weight="bold" size={25} />
-                </button>
-              </p>
-              <p className="bg-[#CECFD2] p-2 w-[15vw] ml-4 text-[#303740] flex flex-row items-center justify-between rounded-lg">
-                {resAPI.estabelecimento.complemento == null ? "****" : resAPI.estabelecimento.complemento}
-                <button
-                  onClick={() =>
-                    copyToClipboard(resAPI.estabelecimento.complemento)
-                  }
-                >
-                  <Copy weight="bold" size={25} />
-                </button>
-              </p>
+            <div className="flex flex-row">
+              <div className="flex flex-col mt-5 text-start w-[30vw]">
+                <div className="flex">
+                  <p className="w-[15vw] text-[#303740]"> Bairro:</p>
+                  <p className=" ml-4 text-[#303740]"> Complemento:</p>
+                </div>
+                <div className="flex flex-row">
+                  <p className="bg-[#CECFD2] p-2 w-[15vw]  text-[#303740] flex flex-row items-center justify-between rounded-lg">
+                    {resAPI.estabelecimento.bairro}
+                    <button
+                      onClick={() =>
+                        copyToClipboard(resAPI.estabelecimento.bairro)
+                      }
+                    >
+                      <Copy weight="bold" size={25} />
+                    </button>
+                  </p>
+                  <p className="bg-[#CECFD2] p-2 w-[15vw] ml-4 text-[#303740] flex flex-row items-center justify-between rounded-lg">
+                    {resAPI.estabelecimento.complemento == null
+                      ? "****"
+                      : resAPI.estabelecimento.complemento}
+                    <button
+                      onClick={() =>
+                        copyToClipboard(resAPI.estabelecimento.complemento)
+                      }
+                    >
+                      <Copy weight="bold" size={25} />
+                    </button>
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="flex flex-row mt-5 text-center w-[30vw]">
-              <p className="bg-[#CECFD2] p-2 w-[10vw]  text-[#303740] flex flex-row items-center justify-between rounded-lg">
-                {resAPI.estabelecimento.cidade.nome}
-                <button onClick={() => copyToClipboard(resAPI.estabelecimento.cidade.nome)}>
-                  <Copy weight="bold" size={25} />
-                </button>
-              </p>
-              <p className="bg-[#CECFD2] p-2 w-[10vw] ml-4 text-[#303740] flex flex-row items-center justify-between rounded-lg">
-                {resAPI.estabelecimento.estado.sigla}
-                <button onClick={() => copyToClipboard(resAPI.estabelecimento.estado.sigla)}>
-                  <Copy weight="bold" size={25} />
-                </button>
-              </p>
-              <p className="bg-[#CECFD2] p-2 w-[10vw] ml-4 text-[#303740] flex flex-row items-center justify-between rounded-lg">
-                {resAPI.estabelecimento.cep}
-                <button onClick={() => copyToClipboard(resAPI.estabelecimento.cep)}>
-                  <Copy weight="bold" size={25} />
-                </button>
-              </p>
+            <div className="flex flex-col mt-5 text-start w-[30vw]">
+              <div className="flex">
+                <p className="w-[10vw] text-[#303740]"> Cidade:</p>
+                <p className=" ml-4 w-[10vw] text-[#303740]"> Estado:</p>
+                <p className=" ml text-[#303740]"> CEP:</p>
+              </div>
+              <div className="flex flex-row">
+                <p className="bg-[#CECFD2] p-2 w-[10vw]  text-[#303740] flex flex-row items-center justify-between rounded-lg">
+                  {resAPI.estabelecimento.cidade.nome}
+                  <button
+                    onClick={() =>
+                      copyToClipboard(resAPI.estabelecimento.cidade.nome)
+                    }
+                  >
+                    <Copy weight="bold" size={25} />
+                  </button>
+                </p>
+                <p className="bg-[#CECFD2] p-2 w-[10vw] ml-4 text-[#303740] flex flex-row items-center justify-between rounded-lg">
+                  {resAPI.estabelecimento.estado.sigla}
+                  <button
+                    onClick={() =>
+                      copyToClipboard(resAPI.estabelecimento.estado.sigla)
+                    }
+                  >
+                    <Copy weight="bold" size={25} />
+                  </button>
+                </p>
+                <p className="bg-[#CECFD2] p-2 w-[10vw] ml-4 text-[#303740] flex flex-row items-center justify-between rounded-lg">
+                  {resAPI.estabelecimento.cep}
+                  <button
+                    onClick={() => copyToClipboard(resAPI.estabelecimento.cep)}
+                  >
+                    <Copy weight="bold" size={25} />
+                  </button>
+                </p>
+              </div>
             </div>
-            <div className="flex flex-row mt-5 text-center w-[30vw]">
-              <p className="bg-[#CECFD2] p-2 w-[15vw]  text-[#303740] flex flex-row items-center justify-between rounded-lg">
-                {resAPI.estabelecimento.inscricoes_estaduais[0].inscricao_estadual}
-                <button onClick={() => copyToClipboard("Nada Consta")}>
-                  <Copy weight="bold" size={25} />
-                </button>
-              </p>
-              <p className="bg-[#CECFD2] p-2 w-[15vw] ml-4 text-[#303740] flex flex-row items-center justify-between rounded-lg">
-                {resAPI.estabelecimento.situacao_cadastral}
-                <button
-                  onClick={() =>
-                    copyToClipboard(resAPI.estabelecimento.situacao_cadastral)
-                  }
-                >
-                  <Copy weight="bold" size={25} />
-                </button>
-              </p>
+            <div className="flex flex-col mt text-start w-[30vw]">
+            <div className="flex mt-5 ">
+                <p className="w-[15vw] text-[#303740]"> País:</p>
+                <p className="text-[#303740] ml-2"> Situação Cadastral:</p>
+              </div>
+              <div className="flex flex-row ">
+                <p className="bg-[#CECFD2] p-2 w-[15vw]  text-[#303740] flex flex-row items-center justify-between rounded-lg">
+                  {resAPI.estabelecimento.pais.nome}
+                  <button
+                    onClick={() =>
+                      copyToClipboard(resAPI.estabelecimento.pais.nome)
+                    }
+                  >
+                    <Copy weight="bold" size={25} />
+                  </button>
+                </p>
+                <p className="bg-[#CECFD2] p-2 w-[15vw] ml-4 text-[#303740] flex flex-row items-center justify-between rounded-lg">
+                  {resAPI.estabelecimento.situacao_cadastral}
+                  <button
+                    onClick={() =>
+                      copyToClipboard(resAPI.estabelecimento.situacao_cadastral)
+                    }
+                  >
+                    <Copy weight="bold" size={25} />
+                  </button>
+                </p>
+              </div>
             </div>
           </div>
         </Modal>
